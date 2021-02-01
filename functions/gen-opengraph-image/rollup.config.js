fc9ce3c856d74092db3b61f230928df2fa0f7710
Parker <parkerjlandon@gmail.com>
2021-01-31 23:38:26 -0800

    rollup image

--- functions/gen-opengraph-image/rollup.config.js
+++ functions/gen-opengraph-image/rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import builtins from "rollup-plugin-node-builtins";
 import globals from "rollup-plugin-node-globals";
 import replace from "@rollup/plugin-replace";
+import image from "@rollup/plugin-image";
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -33,7 +34,10 @@ const config = {
       "process.env.NODE_ENV": JSON.stringify("production")
     }),
     globals(),
-    builtins()
+    builtins(),
+    image({
+      dom: true
+    })
   ]
 };
 
